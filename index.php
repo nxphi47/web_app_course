ccc854d86b6eaed3420facb69556c95c3f91e1a1
phi <nxphi47@gmail.com>
2018-10-04 04:19:25 +0800

    first commit

--- index.php
+++ index.php
@@ -35,6 +35,71 @@ include "header.php"
 
 <div class="content">
 
+    <div class="slideshow-wrapper" id="slideshow-wrapper">
+        <div class="slideshow">
+            <div class="slide fade">
+                <div class="number-text">1 / 3</div>
+                <img class="slide-img" src="imgs/pizza_test.jpg">
+                <div class="caption">Caption Text 1</div>
+            </div>
+
+            <div class="slide fade">
+                <div class="number-text">2 / 3</div>
+                <img class="slide-img" src="imgs/pizza_test_2.jpeg">
+                <div class="caption">Caption Text 2</div>
+            </div>
+
+            <div class="slide fade">
+                <div class="number-text">3 / 3</div>
+                <img class="slide-img" src="imgs/pizza_test.jpg">
+                <div class="caption">Caption Text 3</div>
+            </div>
+
+            <!-- Next and previous buttons -->
+            <a class="prev" onclick="plusSlides(-1)">&#10094;</a>
+            <a class="next" onclick="plusSlides(1)">&#10095;</a>
+        </div>
+        <br>
+
+        <!-- The dots/circles -->
+        <div style="text-align:center">
+            <span class="dot" onclick="currentSlide(1)"></span>
+            <span class="dot" onclick="currentSlide(2)"></span>
+            <span class="dot" onclick="currentSlide(3)"></span>
+        </div>
+    </div>
+
+    <script>
+        var slideIndex = 1;
+        showSlides(slideIndex);
+
+        // Next/previous controls
+        function plusSlides(n) {
+            showSlides(slideIndex += n);
+        }
+
+        // Thumbnail image controls
+        function currentSlide(n) {
+            showSlides(slideIndex = n);
+        }
+
+        function showSlides(n) {
+            var i;
+            var slides = document.getElementsByClassName("slide");
+            var dots = document.getElementsByClassName("dot");
+            if (n > slides.length) {slideIndex = 1}
+            if (n < 1) {slideIndex = slides.length}
+            for (i = 0; i < slides.length; i++) {
+                slides[i].style.display = "none";
+            }
+            for (i = 0; i < dots.length; i++) {
+                dots[i].className = dots[i].className.replace(" active", "");
+            }
+            slides[slideIndex-1].style.display = "block";
+            dots[slideIndex-1].className += " active";
+        }
+    </script>
+
     <div class="item-list-wrapper" id="item-list-wrapper"></div>
 
     <br>
